cfcf1f7ff9cf84795cbb4b83cd5270602d648168
root <opddr@naver.com>
2018-07-28 11:40:11 -0400

    pcap

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char* argv[]) {
   struct ip *ip_head;
   struct ether_header *eth;
   struct tcphdr *tcp;
+  int ipsize;
   char *addr;
   char* dev = argv[1];
   char errbuf[PCAP_ERRBUF_SIZE];
@@ -38,21 +39,42 @@ int main(int argc, char* argv[]) {
     packet+=sizeof(struct ether_header);
     printf("============================================\n");
     printf("src mac - %x:%x:%x:%x:%x:%x\n",eth->ether_shost[0],eth->ether_shost[1],eth->ether_shost[2],eth->ether_shost[3],eth->ether_shost[4],eth->ether_shost[5]);
+
     printf("dst mac - %x:%x:%x:%x:%x:%x\n",eth->ether_dhost[0],eth->ether_dhost[1],eth->ether_dhost[2],eth->ether_dhost[3],eth->ether_dhost[4],eth->ether_dhost[5]);
+	
+    printf("ether_type = 0x%X\n",eth->ether_type);
+    if( eth->ether_type != 0x08 )
+    {
+	printf("============================================\n");
+	continue;
+    }
+	
+
     ip_head = (struct ip *)packet;
-    packet += sizeof(struct ip);
+    packet += ip_head->ip_hl * 4;
+
     
     addr = inet_ntoa(ip_head->ip_src);   
     printf("src ip : %s\n",addr);
- 
     addr = inet_ntoa(ip_head->ip_dst);   
     printf("dst ip : %s\n",addr);
-    
+
+
+    printf("ip_protocol = 0x%X \n",ip_head->ip_p);
+    if( ip_head->ip_p != 6 )    
+    {
+	printf("============================================\n");
+	continue;
+    }
+
+
     tcp = (struct tcphdr *)packet;
-    packet += sizeof(struct tcphdr);
-    
-    printf("srcport : %d\ndstport : %d\n",tcp->th_sport,tcp->th_dport);
-    printf("data : %s\n",packet);
+    packet += tcp->th_off * 4;
+
+    ipsize = (int)(ip_head->ip_len) - (ip_head->ip_hl * 4 + tcp->th_off * 4) ;
+    printf("srcport : %hu\ndstport : %hu\n\n",htons(tcp->th_sport),htons(tcp->th_dport));
+    for(int i = 0;i< ipsize; i++ )
+	    putchar(packet[i]);
 
     printf("============================================\n");
 
